69fb64b9b2e25b5a986681d944141314439f96a0
lanibread <brendikali@gmail.com>
2026-04-23 23:06:17 -0400

    moved component

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home';
+import RepetitionExercise from './screens/RepetitionExercise';
+import DurationExercise from './screens/DurationExercise';
+
+const Stack = createStackNavigation();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="RepetitionExercise" component={RepetitionExercise} options={({ route }) => ({ title: route.params.name })} />
+        <Stack.Screen name="DurationExercise" component={DurationExercise} options={({ route }) => ({ title: route.params.name })} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
